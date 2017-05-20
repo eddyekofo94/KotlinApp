8296e3a06b958e4827204b0267799aa0f88c6e66
Eddy Ekofo IFUKI <ifuki@lapp.in2p3.fr>
2017-05-20 20:04:16 +0200

    Initial commit

---
+++ src/main/java/com/example/omgandroid/DetailActivityKotlin.kt
@@ -0,0 +1,15 @@
+package com.example.omgandroid
+
+/**
+ * Created by eddyekofo on 20/05/2017.
+ */
+import android.app.Activity
+import android.os.Bundle
+
+class DetailActivityKotlin : Activity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
